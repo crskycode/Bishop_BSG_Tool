ee31441ac841b353c6404763e6df89afdb222d68
Crsky <crskycode@hotmail.com>
2025-04-08 03:32:38 +0800

    Improve CLI

--- Program.cs
+++ Program.cs
@@ -7,12 +7,13 @@ namespace Bishop_BSG_Tool
     {
         static void Main(string[] args)
         {
-            if (args.Length == 0)
+            if (args.Length != 3)
             {
                 Console.WriteLine("Bishop BSG Tool");
                 Console.WriteLine("  -- Created by Crsky");
                 Console.WriteLine("Usage:");
-                Console.WriteLine("  Create    : Bishop_BSG_Tool [image.png|folder]");
+                Console.WriteLine("  Create    : Bishop_BSG_Tool -c [-bpp8|-bpp32] [image.png|folder]");
+                Console.WriteLine("  Extract   : Bishop_BSG_Tool -e [-bpp8|-bpp32] [image.bsg|folder]");
                 Console.WriteLine();
                 Console.WriteLine("Help:");
                 Console.WriteLine("  This tool is only works with 'BSS-Graphics' files,");
@@ -23,18 +24,77 @@ namespace Bishop_BSG_Tool
                 return;
             }
 
-            var path = Path.GetFullPath(args[0]);
+            var mode = args[0];
+            var path = Path.GetFullPath(args[2]);
 
-            if (Utility.PathIsFolder(path))
+            var bpp = 0;
+
+            if (args[1] == "-bpp8")
             {
-                foreach (var item in Directory.EnumerateFiles(path, "*.png"))
-                {
-                    Convert(item);
-                }
+                bpp = 8;
+            }
+            else if (args[1] == "-bpp32")
+            {
+                bpp = 32;
             }
             else
             {
-                Convert(path);
+                Console.WriteLine("This tool currently only supports bpp8 and bpp32 formats.");
+                return;
+            }
+
+            if (mode == "-c")
+            {
+                if (bpp == 32)
+                {
+                    if (Utility.PathIsFolder(path))
+                    {
+                        foreach (var item in Directory.EnumerateFiles(path, "*.png"))
+                        {
+                            Convert(item);
+                        }
+                    }
+                    else
+                    {
+                        Convert(path);
+                    }
+                }
+                else if (bpp == 8)
+                {
+                    if (Utility.PathIsFolder(path))
+                    {
+                        foreach (var item in Directory.EnumerateFiles(path, "*.png"))
+                        {
+                            ConvertBpp8(item);
+                        }
+                    }
+                    else
+                    {
+                        ConvertBpp8(path);
+                    }
+                }
+            }
+            else if (mode == "-e")
+            {
+                if (bpp == 8)
+                {
+                    if (Utility.PathIsFolder(path))
+                    {
+                        foreach (var item in Directory.EnumerateFiles(path, "*.bsg"))
+                        {
+                            ExtractBpp8(item);
+                        }
+                    }
+                    else
+                    {
+                        ExtractBpp8(path);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Extract mode currently only supports the BPP8 format.");
+                    return;
+                }
             }
 
             Console.WriteLine("Done");
@@ -50,6 +110,42 @@ namespace Bishop_BSG_Tool
                 Console.WriteLine($"Converting {pngName}");
 
                 BSG.Create(bsgPath, filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        static void ConvertBpp8(string filePath)
+        {
+            try
+            {
+                var bsgPath = Path.ChangeExtension(filePath, ".bsg");
+                var pngName = Path.GetFileName(filePath);
+
+                Console.WriteLine($"Converting {pngName}");
+
+                BSG.CreateBpp8(bsgPath, filePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+        }
+
+        static void ExtractBpp8(string filePath)
+        {
+            try
+            {
+                var pngPath = Path.ChangeExtension(filePath, ".png");
+                var bsgName = Path.GetFileName(filePath);
+
+                Console.WriteLine($"Converting {bsgName}");
+
+                BSG.ExtractBpp8(pngPath, filePath);
             }
             catch (Exception e)
             {
